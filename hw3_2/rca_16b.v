920eded97ee7f5e3cd9a70a68a6db5f61c0b460b
Ghost <ghost@ghost.moe>
2019-02-15 18:08:14 -0600

    Finished

--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -15,5 +15,11 @@ module rca_16b(A, B, C_in, S, C_out);
     output         C_out;
 
     // YOUR CODE HERE
+    wire[3:0] carry;
+    rca_4b 
+        a0(A[3:0], B[3:0], C_in, S[3:0], carry[0]),
+        a1(A[7:4], B[7:4], carry[0], S[7:4], carry[1]),
+        a2(A[11:8], B[11:8], carry[1], S[11:8], carry[2]),
+        a3(A[15:12], B[15:12], carry[2], S[15:12], C_out);
 
 endmodule
--- hw3_2/rca_4b.v
+++ hw3_2/rca_4b.v
@@ -15,5 +15,12 @@ module rca_4b(A, B, C_in, S, C_out);
     output         C_out;
 
     // YOUR CODE HERE
+    wire[2:0] carry; 
+    fullAdder_1b 
+        fa0(A[0], B[0], C_in, S[0], carry[0]),
+        fa1(A[1], B[1], carry[0], S[1], carry[1]),
+        fa2(A[2], B[2], carry[1], S[2], carry[2]),
+        fa3(A[3], B[3], carry[2], S[3], C_out);
+
 
 endmodule
